45217ca07753567f38f12bc7bf11ee5ac03c0257
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-10-29 11:34:03 +0000

    fix unit tests

--- tests/ToolbarButton.test.ts
+++ tests/ToolbarButton.test.ts
@@ -2,8 +2,7 @@
  * @jest-environment jsdom
  */
 import { ToolbarButton } from '../src/ToolbarButton';
-import {Messages} from "../src/Enum/Messages";
-import {Toolbar} from "../src/Toolbar";
+import { Messages } from '../src/Enum/Messages';
 
 afterEach(() => {
     document.body.innerHTML = '';
@@ -11,11 +10,11 @@ afterEach(() => {
 
 describe('Class: ToolbarButton', () => {
     it('should create an instance of ToolbarButton', () => {
-        expect(new ToolbarButton({name: 'test', displayName: 'Test'})).toBeInstanceOf(ToolbarButton);
+        expect(new ToolbarButton({name: 'test', title: 'Test'})).toBeInstanceOf(ToolbarButton);
     });
 
     it('should create a HTMLButtonElement', () => {
-        let toolbarButton = new ToolbarButton({name: 'test', displayName: 'Test'});
+        let toolbarButton = new ToolbarButton({name: 'test', title: 'Test'});
 
         expect(toolbarButton.getButton()).toBeInstanceOf(HTMLButtonElement);
         expect(toolbarButton.getButton().type).toBe('button');
@@ -23,7 +22,7 @@ describe('Class: ToolbarButton', () => {
         expect(toolbarButton.getButton().innerHTML).toBe('Test');
 
         let formElement = document.createElement('form'),
-            formToolbarButton = new ToolbarButton({name: 'test', displayName: 'Test'});
+            formToolbarButton = new ToolbarButton({name: 'test', title: 'Test'});
 
         formElement.append(formToolbarButton.getButton());
         document.body.append(formElement);
@@ -33,11 +32,11 @@ describe('Class: ToolbarButton', () => {
         expect(formToolbarButton.getButton().innerHTML).toBe('Test');
     });
 
-    it('throws an error when display name is empty and icon classes are empty', () => {
+    it('throws an error when title is empty and icon classes are empty', () => {
         expect(() => {
             new ToolbarButton({
                 name: 'test',
-                displayName: '',
+                title: '',
             });
         }).toThrowError(Messages.ERROR_NO_TOOLBAR_BUTTON_HTML_PROVIDED);
     });
@@ -45,7 +44,7 @@ describe('Class: ToolbarButton', () => {
     it('should create FontAwesome icon when provided classes', () => {
         let boldButton = new ToolbarButton({
             name: 'bold',
-            displayName: '',
+            title: 'Bold',
             iconClasses: [
                 'fa-solid',
                 'fa-bold',
